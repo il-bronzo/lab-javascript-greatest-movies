b32673367fc4da7a3024a4f1cd8d099c485ea851
Andrea Bronzoni <bronzoni.andrea@libero.it>
2024-10-08 16:18:50 +0200

    iteration 5 completed

--- src/movies.js
+++ src/movies.js
@@ -49,7 +49,14 @@ function dramaMoviesScore(moviesArray) {
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    const copyMovies = moviesArray.map((elem) => elem);
+    copyMovies.sort((a, b) => {
+        if (a.year !== b.year) return a.year - b.year;
+        return a.title.localeCompare(b.title);
+    });
+    return copyMovies;
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
